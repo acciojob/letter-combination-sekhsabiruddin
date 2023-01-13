ab6ff24c8e21f56436d12fe81cd2b59cd16c5c12
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-13 15:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let obj ={
+		2:"abc",
+		3:"def",
+		5:"ghi",
+		6:"jkl",
+		7:"mno",
+		8:"tuv",
+		9:"wxyz"
+	}
+	let ans =obj[input_digit];
+	return ans;
+	
 }
 
 module.exports = letterCombinations;
